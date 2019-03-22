b937dbb5294dec9d335ad6e5f706240ba23c2bc2
Vladimir Gourov <vladimir.gourov@blackrock.com>
2019-03-22 02:37:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function takeANumber(line, name){
 
 function nowServing(line){
     if(line.length === 0){
-        return `There's nobody waiting to be served!`
+        return `There is nobody waiting to be served!`
     }
 }
