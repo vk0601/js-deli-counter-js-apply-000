8c14c332c3a57f64fb4fd0fe93090fa7b936ee95
Vladimir Gourov <vladimir.gourov@blackrock.com>
2019-03-22 02:57:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ function nowServing(line){
         return `There is nobody waiting to be served!`
     } return `Currently serving ${line.shift(name)}.`
 }
+
+function currentLine(line){
+    if(line.length === 0){
+        return "The line is currently empty."
+    }
+}
